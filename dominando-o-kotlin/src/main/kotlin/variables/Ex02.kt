3d5f34d7125a2050874e6f5383b1498c85a69bc9
George Salu <george.salu10@gmail.com>
2025-04-22 14:45:41 -0300

    dominando-o-kotlin - variaveis e tipos de dados - exercicio truncando um valor fornecido pelo usuario

---
+++ dominando-o-kotlin/src/main/kotlin/variables/Ex02.kt
@@ -0,0 +1,14 @@
+package variables
+
+/*
+* Exercicio
+*
+* Crie um programa que arredona uma valor decimal informado pelo usuario e mostra a resposta na tela
+* */
+fun main() {
+
+    print("> ")
+    val x = readln().toDouble().toInt()
+
+    println(x)
+}
