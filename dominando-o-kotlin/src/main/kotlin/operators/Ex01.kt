b818844fa19577c32da1fe672d963ff0b7bb6125
George Salu <george.salu10@gmail.com>
2025-04-22 15:53:12 -0300

    dominando-o-kotlin - operadores - resolvendo uma expressao com varios operadores

---
+++ dominando-o-kotlin/src/main/kotlin/operators/Ex01.kt
@@ -0,0 +1,12 @@
+package operators
+
+/*
+* Encontre o resultado da seguinte expressao
+* (5 + 3) * 2 < 30 / 2 || !(5 - 1 <= 8 % 3)
+* */
+fun main() {
+
+    val exp = (5 + 3) * 2 < 30 / 2 || !(5 - 1 <= 8 % 3)
+    println(exp)
+
+}
