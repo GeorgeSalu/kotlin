9a87e5d597d6dd90e488328fa0577b645a352321
George Salu <george.salu10@gmail.com>
2025-04-22 15:56:09 -0300

    dominando-o-kotlin - operadores - resolvendo outra expressao com varios operadores

---
+++ dominando-o-kotlin/src/main/kotlin/operators/Ex02.kt
@@ -0,0 +1,14 @@
+package operators
+
+/*
+* Exercicio
+*
+* Encontre o resultado da seguinte expressao
+* !(2 * 4 >= 16 / 2 && 5 == 4 + 1)
+* */
+fun main() {
+
+    val exp = !(2 * 4 >= 16 / 2 && 5 == 4 + 1)
+    println(exp)
+
+}
