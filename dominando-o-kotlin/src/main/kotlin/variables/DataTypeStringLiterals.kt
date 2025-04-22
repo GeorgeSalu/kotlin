6db7d7b20dc24b29a7b3ede0f650bd3a2f35e318
George Salu <george.salu10@gmail.com>
2025-04-22 14:19:39 -0300

    dominando-o-kotlin - variaveis e tipos de dados - tipo string literais parte 2

--- dominando-o-kotlin/src/main/kotlin/variables/DataTypeStringLiterals.kt
+++ dominando-o-kotlin/src/main/kotlin/variables/DataTypeStringLiterals.kt
@@ -3,6 +3,15 @@ package variables
 fun main() {
 
     val s1 = "O rato roeu\na \$roupa do\nrei de \"roma\"."
+
+    val s2 = """
+        O rato roeu
+        a roupa do 
+        rei de "roma"
+    """.trimIndent()
+
     println(s1)
+    println()
+    println(s2)
 
 }
