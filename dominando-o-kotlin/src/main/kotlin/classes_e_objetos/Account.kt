396731a79f2761f2ab341b0bef848de9716f322e
George Salu <george.salu10@gmail.com>
2025-04-23 17:00:12 -0300

    dominando-o-kotlin - classes e objetos - passando parametros no construtor da classe

--- dominando-o-kotlin/src/main/kotlin/classes_e_objetos/Account.kt
+++ dominando-o-kotlin/src/main/kotlin/classes_e_objetos/Account.kt
@@ -1,11 +1,17 @@
 package classes_e_objetos
 
-class Account {
+class Account(number: String = "", owner: String = "") {
 
+    val accountNumber: String = number
+    val accountOwner: String = owner
     var balance: Double = 0.0
 
     fun deposit(amount: Double) {
         balance += amount
+    }
+
+    fun print() {
+        println("NUmber: $accountNumber; Owner: $accountOwner; Balance: $balance")
     }
 
 }
--- dominando-o-kotlin/src/main/kotlin/classes_e_objetos/App1.kt
+++ dominando-o-kotlin/src/main/kotlin/classes_e_objetos/App1.kt
@@ -2,12 +2,12 @@ package classes_e_objetos
 
 fun main() {
 
-    val a1 = Account()
-    val a2 = Account()
+    val a1 = Account("1234-1", "joao")
+    val a2 = Account("4445-5", "maria")
 
     a1.deposit(2000.0)
     a2.deposit(150.0)
 
-    println(a1.balance)
-    println(a2.balance)
+    println(a1.print())
+    println(a2.print())
 }
