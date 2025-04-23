c1b22ae25e7bb79cab8430a08a3409e8b6a45ee2
George Salu <george.salu10@gmail.com>
2025-04-23 09:31:01 -0300

    dominando-o-kotlin - estrutura de repeticao - estrutura de repeticao for

---
+++ dominando-o-kotlin/src/main/kotlin/estrutura_repeticao/For.kt
@@ -0,0 +1,19 @@
+package estrutura_repeticao
+
+fun main() {
+
+    for (i in 0..10) {
+        print("$i ")
+    }
+
+    println()
+    for (i in 10 downTo 0 step 2) {
+        print("$i ")
+    }
+
+    println()
+    for (i in 10 until 100 step 3) {
+        print("$i ")
+    }
+
+}
