2eebe702d8e96e76dc9d623bc6b5d18c7adab8b1
George Salu <george.salu10@gmail.com>
2025-04-22 21:26:33 -0300

    dominando-o-kotlin - estrutura de selecao - outro tipo de uso do when

--- dominando-o-kotlin/src/main/kotlin/selecao/When2.kt
+++ dominando-o-kotlin/src/main/kotlin/selecao/When2.kt
@@ -5,7 +5,7 @@ fun main() {
     val x = 10
 
     when {
-        x > 0 -> println("prositivo")
+        x > 0 -> println("positivo")
         x < 0 -> println("negativo")
         else -> println("zero")
     }
