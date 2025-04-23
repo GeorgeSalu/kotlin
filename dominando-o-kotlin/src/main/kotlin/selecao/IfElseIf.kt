a7d0ee93d034f899806de723f25ff72f1c72f22f
George Salu <george.salu10@gmail.com>
2025-04-22 21:01:24 -0300

    dominando-o-kotlin - estrutura de selecao - anilhando estruturas if usando o else if

---
+++ dominando-o-kotlin/src/main/kotlin/selecao/IfElseIf.kt
@@ -0,0 +1,15 @@
+package selecao
+
+fun main() {
+
+    val v = 10
+    
+    if (v > 0) {
+        println("positivo")
+    } else if (v < 0) {
+        println("negativo")
+    } else {
+        println("zero")
+    }
+
+}
