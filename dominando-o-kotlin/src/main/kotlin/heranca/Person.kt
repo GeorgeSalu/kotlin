abb5973c742bf0f13989f46623ac344d6a077473
George Salu <george.salu10@gmail.com>
2025-04-24 14:37:43 -0300

    dominando-o-kotlin - heranca - criando data class parte 2

--- dominando-o-kotlin/src/main/kotlin/heranca/Person.kt
+++ dominando-o-kotlin/src/main/kotlin/heranca/Person.kt
@@ -2,19 +2,20 @@ package heranca
 
 fun main() {
 
-    val person1 = Person("Pedro", "Oliveira", 25)
-    val person2 = Person("Pedro", "Oliveira", 25)
+    val person1 = Person("Pedro", "Oliveira")
+    val person2 = Person("Pedro", "Oliveira")
 
     println(person1)
     println(person2)
     println(person1 == person2)
 
-    val person3 = person2.copy(age = 30)
+    val person3 = person2.copy()
     println(person3)
 }
 
 data class Person (
     val firstName: String,
     val lastName: String,
-    val age: Int
-)
+) {
+    val age: Int = 10
+}
