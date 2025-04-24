cb6e4831891a172fdf9d546c48b3c9ad8b019806
George Salu <george.salu10@gmail.com>
2025-04-24 12:14:54 -0300

    dominando-o-kotlin - heranca - o operador is

--- dominando-o-kotlin/src/main/kotlin/heranca/App1.kt
+++ dominando-o-kotlin/src/main/kotlin/heranca/App1.kt
@@ -4,5 +4,15 @@ fun main() {
 
     val animal = Animal("branco")
     val dog = Dog("preto")
+    val cat = Cat("Cinza")
+
+    var a: Animal = dog
+    var b: Animal = cat
+
+    println(animal is Animal)
+    println(dog is Dog)
+    println(dog is Animal)
+    println(cat is Animal)
+
 
 }
