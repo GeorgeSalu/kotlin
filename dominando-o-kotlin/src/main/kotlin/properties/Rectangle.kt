08e614319d743e2cafa3dec8ec025fb21e8eead4
George Salu <george.salu10@gmail.com>
2025-04-24 10:30:41 -0300

    dominando-o-kotlin - properties - entendendo como uma propriedade funciona internamente

---
+++ dominando-o-kotlin/src/main/kotlin/properties/Rectangle.kt
@@ -0,0 +1,23 @@
+package properties
+
+fun main() {
+
+    val r1 = Rectangle()
+    r1.width = 10
+    println(r1.width)
+
+}
+
+class Rectangle {
+
+    var width: Int = 0
+        get() {
+            return field * 2
+        }
+        set(value) {
+            if(value >= 0) {
+                field = value
+            }
+        }
+
+}
