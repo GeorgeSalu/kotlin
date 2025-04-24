d5bb89c44ec4b7a8fb47b4876f4401825b9e4b24
George Salu <george.salu10@gmail.com>
2025-04-24 12:33:14 -0300

    dominando-o-kotlin - heranca - herdando metodos e propriedades da superclasse

---
+++ dominando-o-kotlin/src/main/kotlin/heranca/App2.kt
@@ -0,0 +1,15 @@
+package heranca
+
+fun main() {
+
+    val cat = Cat("Branco")
+    val dog = Dog("Preto")
+
+    cat.miau()
+    cat.eat()
+    println(cat.color)
+
+    dog.bark()
+    dog.eat()
+    println(dog.color)
+}
