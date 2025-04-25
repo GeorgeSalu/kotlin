45358b75c9d4559f6907b99d143ae956417aa377
George Salu <george.salu10@gmail.com>
2025-04-25 11:19:19 -0300

    dominando-o-kotlin - gerenciamento de excecoes - lancando uma illegalArgumentException com o metodo require

---
+++ dominando-o-kotlin/src/main/kotlin/exceptions/StudentGrades.kt
@@ -0,0 +1,32 @@
+package exceptions
+
+fun main() {
+
+    val avg = StudentGrades()
+        .add(3.0)
+        .add(5.5)
+        .add(7.0)
+        .average()
+
+    println(avg)
+}
+
+class StudentGrades {
+    private var sum: Double = 0.0
+    private var numberOfGrades: Int = 0
+
+    fun add(grade: Double): StudentGrades {
+
+        if (grade !in 0.0..10.0 ) {
+            throw IllegalArgumentException("Invalid")
+        }
+
+        sum += grade
+        numberOfGrades++
+        return this
+    }
+
+    fun average() : Double {
+        return  sum / numberOfGrades
+    }
+}
