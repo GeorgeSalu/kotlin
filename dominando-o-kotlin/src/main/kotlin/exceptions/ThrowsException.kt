2f33c9eedab7a1f08bb52f263a3b095ac7a44678
George Salu <george.salu10@gmail.com>
2025-04-25 10:30:09 -0300

    dominando-o-kotlin - gerenciamento de excecoes - lancando uma excecao com throws

---
+++ dominando-o-kotlin/src/main/kotlin/exceptions/ThrowsException.kt
@@ -0,0 +1,26 @@
+package exceptions
+
+fun main() {
+
+    val account = BankAccount()
+    account.deposit(-500.0)
+    //account.withdraw(100.0)
+    println(account.balance)
+
+}
+
+class BankAccount() {
+    var balance: Double = 0.0
+        private  set
+
+    fun deposit(value: Double) {
+        if (value < 0) {
+            throw Exception("value cannot be negative")
+        }
+        balance += value
+    }
+
+    fun withdraw(value: Double) {
+        balance -= value
+    }
+}
