e3c25915db7eb7b9bd31933534f1199aa700a8a6
George Salu <george.salu10@gmail.com>
2025-04-26 10:24:17 -0300

    dominando-o-kotlin - expressoes lambda - exercicio: criando uma função que imprime no console

---
+++ dominando-o-kotlin/src/main/kotlin/lambdas/ExConsolePrinter.kt
@@ -0,0 +1,12 @@
+package lambdas
+
+/**
+ * EXERCÍCIO
+ * ---------
+ *
+ * Implemente uma função que imprime informações na tela entre duas linhas que contêm asteriscos (**********). O
+ * conteúdo impresso deve ser computado por uma expressão lambda.
+ */
+fun main() {
+
+}
