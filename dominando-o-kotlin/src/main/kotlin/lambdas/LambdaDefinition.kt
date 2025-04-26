87aa2694997376e7cc981998de1e29ffcc659f9f
George Salu <george.salu10@gmail.com>
2025-04-25 21:51:49 -0300

    dominando-o-kotlin - expressoes lambda - definindo sua primeira expressao lambda

--- dominando-o-kotlin/src/main/kotlin/lambdas/LambdaDefinition.kt
+++ dominando-o-kotlin/src/main/kotlin/lambdas/LambdaDefinition.kt
@@ -13,6 +13,10 @@ fun main() {
 
     val r3 = f1(10, 5)
     println(r3)
+
+    val f2: (Int, Int) -> Int = { a: Int, b: Int -> a + b }
+    val r4 = f2(10, 5)
+    println(r4)
 }
 
 fun sum(a: Int,b: Int): Int {
