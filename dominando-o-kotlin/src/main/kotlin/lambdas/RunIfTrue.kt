8b80c835f476d53e2ed5434351fc3ea003be12e7
George Salu <george.salu10@gmail.com>
2025-04-26 11:49:38 -0300

    dominando-o-kotlin - expressoes lambda - exercicio: criando a funcao runIfTrue() (solucao)

--- dominando-o-kotlin/src/main/kotlin/lambdas/RunIfTrue.kt
+++ dominando-o-kotlin/src/main/kotlin/lambdas/RunIfTrue.kt
@@ -12,4 +12,20 @@ package lambdas
  */
 fun main() {
 
+    val x = 10
+
+    val r = runIfTrue(x > 5) {
+        println("code executed")
+        x
+    }
+
+    println(r)
+}
+
+fun runIfTrue(condition: Boolean, code: () -> Any?): Any? {
+    return if(condition) {
+        code()
+    } else {
+        null
+    }
 }
