c2fb7568e817680d177ee61efbcbfce3f6d6f295
George Salu <george.salu10@gmail.com>
2025-04-26 11:25:04 -0300

    dominando-o-kotlin - expressoes lambda - exercicio: criando a funcao runIfTrue()

---
+++ dominando-o-kotlin/src/main/kotlin/lambdas/RunIfTrue.kt
@@ -0,0 +1,15 @@
+package lambdas
+
+/**
+ * EXERCÍCIO
+ * ---------
+ *
+ * Crie uma função chamada runIfTrue() que recebe um valor booleano e um código a ser executado caso o booleano
+ * seja verdadeiro (se ele for falso o código não será executado).
+ *
+ * A função runIfTrue() deve retornar o valor retornado pelo código executado, ou null caso o código não tenha sido
+ * executado.
+ */
+fun main() {
+
+}
