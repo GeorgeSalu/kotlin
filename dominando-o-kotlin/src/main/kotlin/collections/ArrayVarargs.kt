b2945c430e0cb296c69884cebaa341c2afd1a597
George Salu <george.salu10@gmail.com>
2025-04-26 15:49:48 -0300

    dominando-o-kotlin - colecoes de dados - variando o numero de parametros com o varargs

---
+++ dominando-o-kotlin/src/main/kotlin/collections/ArrayVarargs.kt
@@ -0,0 +1,20 @@
+package collections
+
+fun main() {
+
+    println(sum(2, 5, 20, 30,4))
+
+    val a = intArrayOf(2,3,4,6,7,3)
+    println(sum(*a))
+}
+
+fun subtraction(n1: Int,n2: Int): Int {
+    return n1 - n2
+}
+
+fun sum(vararg  values: Int): Int {
+    var sum = 0
+    values.forEach { sum += it }
+    return sum
+}
+
