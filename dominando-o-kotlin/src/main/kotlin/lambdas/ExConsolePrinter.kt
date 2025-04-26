7aa26b667f417ecc2d7ca51c434ced84e4b9bc5d
George Salu <george.salu10@gmail.com>
2025-04-26 10:51:49 -0300

    dominando-o-kotlin - expressoes lambda - exercicio: criando uma função que imprime no console (solucao)

--- dominando-o-kotlin/src/main/kotlin/lambdas/ExConsolePrinter.kt
+++ dominando-o-kotlin/src/main/kotlin/lambdas/ExConsolePrinter.kt
@@ -9,4 +9,17 @@ package lambdas
  */
 fun main() {
 
+    console { "Hey!" }
+
+    val x = 10
+    console {
+        val y = (x * 2) * -1
+        y + 1
+    }
+}
+
+fun console(producer: () -> Any) {
+    println("********************")
+    println(producer())
+    println("********************")
 }
