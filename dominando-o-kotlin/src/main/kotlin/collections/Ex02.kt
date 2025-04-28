d16943dd2e1a167f6e95f33363fbe31e81ca4b75
George Salu <george.salu10@gmail.com>
2025-04-28 10:30:01 -0300

    dominando-o-kotlin - colecoes de dados - exercicio: extraindo os paises associdados a codigos telefonicos

---
+++ dominando-o-kotlin/src/main/kotlin/collections/Ex02.kt
@@ -0,0 +1,22 @@
+package collections
+
+/**
+ * EXERCÍCIO
+ * ---------
+ *
+ * Crie um mapa que mapeia códigos telefônicos aos seus países associados. A partir deste mapa, gere uma lista mutável
+ * que contém os países associados a estes códigos ordenados em ordem alfabética decrescente.
+ */
+fun main() {
+
+    val codeMap = mapOf(
+        55 to "Brazil",
+        351 to "Portugal",
+        54 to "Argentina",
+        1 to "United States"
+    )
+    println(codeMap)
+
+    val countries = codeMap.values.toSortedSet(Comparator.reverseOrder()).toMutableList()
+    println(countries)
+}
