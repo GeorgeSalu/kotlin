e91381bcf212258d8601f5df00475cb3e2c6629f
George Salu <george.salu10@gmail.com>
2025-04-28 10:25:31 -0300

    dominando-o-kotlin - colecoes de dados - exercicio: contando cidades unicas numa lista (solucao)

--- dominando-o-kotlin/src/main/kotlin/collections/Ex01.kt
+++ dominando-o-kotlin/src/main/kotlin/collections/Ex01.kt
@@ -9,4 +9,11 @@ package collections
  */
 fun main() {
 
+    val cities = listOf("Londres", "Paris", "Lisboa", "Londres", "Roma", "Roma", "Berlim", "Madri", "Lisboa")
+    println(cities)
+    println(cities.size)
+
+    val uniqueCities = cities.toSet()
+    println(uniqueCities)
+    println(uniqueCities.size)
 }
