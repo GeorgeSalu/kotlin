fee13cc08fd6ca2f87a66168e5604dde7b01cf45
George Salu <george.salu10@gmail.com>
2025-04-28 10:23:26 -0300

    dominando-o-kotlin - colecoes de dados - exercicio: contando cidades unicas numa lista

---
+++ dominando-o-kotlin/src/main/kotlin/collections/Ex01.kt
@@ -0,0 +1,12 @@
+package collections
+
+/**
+ * EXERCÍCIO
+ * ---------
+ *
+ * Crie uma lista de cidades que você visitou hipoteticamente nos últimos meses (a lista pode conter cidades repetidas).
+ * Crie um algoritmo que conta quantas cidades você visitou (isto é, excluindo cidades duplicadas).
+ */
+fun main() {
+
+}
