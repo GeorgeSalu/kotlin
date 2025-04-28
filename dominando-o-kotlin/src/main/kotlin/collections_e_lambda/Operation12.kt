6989334aa65ace39c98243210583810c983bd91d
George Salu <george.salu10@gmail.com>
2025-04-28 14:27:28 -0300

    dominando-o-kotlin - colecoes de dados e lambdas - operacao associate()

---
+++ dominando-o-kotlin/src/main/kotlin/collections_e_lambda/Operation12.kt
@@ -0,0 +1,16 @@
+package collections_e_lambda
+
+/**
+ * OBJETIVO
+ * --------
+ *
+ * Criar um mapa a partir da lista de pessoas, onde o nome da pessoa é a chave e a idade é o valor.
+ */
+fun main() {
+
+    Person
+        .data()
+        .associate { it.name to it.age }
+        .forEach { (n, a) -> println("$n => $a") }
+
+}
