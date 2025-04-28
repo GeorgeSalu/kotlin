b62a7dcc6cc10647a8f27c867efe90cd24f6faad
George Salu <george.salu10@gmail.com>
2025-04-28 12:14:01 -0300

    dominando-o-kotlin - colecoes de dados e lambdas - operacao reduce()

---
+++ dominando-o-kotlin/src/main/kotlin/collections_e_lambda/Operation7.kt
@@ -0,0 +1,17 @@
+package collections_e_lambda
+
+/**
+ * OBJETIVO
+ * --------
+ *
+ * Somar a idade de todas as pessoas.
+ */
+fun main() {
+
+    val sum = Person
+        .data()
+        .map { it.age }
+        .reduce { acc, age -> acc + age }
+
+    println(sum)
+}
