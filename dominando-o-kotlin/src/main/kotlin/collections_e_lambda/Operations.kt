9e136140331897a0dd81c3f4728cbb4d24742c21
George Salu <george.salu10@gmail.com>
2025-04-28 11:06:00 -0300

    dominando-o-kotlin - colecoes de dados e lambdas - operacao filter()

---
+++ dominando-o-kotlin/src/main/kotlin/collections_e_lambda/Operations.kt
@@ -0,0 +1,16 @@
+package collections_e_lambda
+
+/**
+ * OBJETIVO
+ * --------
+ *
+ * Construir uma lista apenas com as pessoas com idade maior ou igual a 18 anos.
+ */
+fun main() {
+
+    Person
+        .data()
+        .filter { it.age >= 18 }
+        .forEachIndexed { i, p -> println("${i + 1}. $p") }
+
+}
