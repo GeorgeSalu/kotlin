5cc135f5377c863858af561f9ec673de1b134dca
George Salu <george.salu10@gmail.com>
2025-04-28 15:44:36 -0300

    dominando-o-kotlin - enums - criando um enum com os meses do ano

---
+++ dominando-o-kotlin/src/main/kotlin/enums/Month.kt
@@ -0,0 +1,23 @@
+package enums
+
+fun main() {
+
+    val month: Month = Month.MAY
+
+    println(month)
+}
+
+enum class Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+}
