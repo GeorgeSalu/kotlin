8914af5f2cad891cf39c619f4070fe55c47b0831
George Salu <george.salu10@gmail.com>
2025-04-28 12:10:52 -0300

    dominando-o-kotlin - colecoes de dados e lambdas - operacao sumOf()

---
+++ dominando-o-kotlin/src/main/kotlin/collections_e_lambda/Operation6.kt
@@ -0,0 +1,17 @@
+package collections_e_lambda
+
+/**
+ * OBJETIVO
+ * --------
+ *
+ * Somar a idade de todas as pessoas cujo nome começa com a letra 'R'.
+ */
+fun main() {
+
+    val count = Person
+        .data()
+        .filter { it.name.uppercase().startsWith("R") }
+        .sumOf { it.age }
+
+    println(count)
+}
