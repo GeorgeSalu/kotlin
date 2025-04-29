c3d793b7fee056c93bf25a38ae6841378836ab52
George Salu <george.salu10@gmail.com>
2025-04-28 21:56:55 -0300

    dominando-o-kotlin - organizacao de codigo - criando apelidos para tipos de dados com typealias

---
+++ dominando-o-kotlin/src/main/kotlin/organization/App3.kt
@@ -0,0 +1,18 @@
+package organization
+
+typealias ExecutorID = String
+typealias CodeToExecute = (ExecutorID) -> String
+
+fun main() {
+
+
+
+}
+
+class Executor(val id: ExecutorID) {
+
+    fun execute(code: CodeToExecute): String {
+        return code(id)
+    }
+
+}
