bc85cb3e5ad65b8f5d8337c74d5e378340d449dd
George Salu <george.salu10@gmail.com>
2025-04-30 09:58:05 -0300

    criando o projeto - primeiros-passos-spring-boot-kotlin

--- primeiros-passos-spring-boot-kotlin/src/main/kotlin/br/com/erudio/StartupApplication.kt
+++ primeiros-passos-spring-boot-kotlin/src/main/kotlin/br/com/erudio/StartupApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class RestWithSpringBootAndKotlinErudioApplication
+class StartupApplication
 
 fun main(args: Array<String>) {
-	runApplication<RestWithSpringBootAndKotlinErudioApplication>(*args)
+	runApplication<StartupApplication>(*args)
 }
