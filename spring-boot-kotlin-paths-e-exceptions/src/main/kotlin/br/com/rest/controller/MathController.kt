1d34ede74640b89eccff5fcfa29ed9f21c5bd718
George Salu <george.salu10@gmail.com>
2025-04-30 19:49:19 -0300

    spring-boot-kotlin-paths-e-exceptions - implementando a operacao division

--- spring-boot-kotlin-paths-e-exceptions/src/main/kotlin/br/com/rest/controller/MathController.kt
+++ spring-boot-kotlin-paths-e-exceptions/src/main/kotlin/br/com/rest/controller/MathController.kt
@@ -29,6 +29,13 @@ class MathController {
         return convertToDouble(numberOne) * convertToDouble(numberTwo)
     }
 
+    @RequestMapping(value = ["/division/{numberOne}/{numberTwo}"])
+    fun division(@PathVariable(value = "numberOne") numberOne: String?, @PathVariable(value = "numberTwo") numberTwo: String?): Double {
+
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw UnsupportedMathOperationException("Please set a numeric value")
+        return convertToDouble(numberOne) / convertToDouble(numberTwo)
+    }
+
     private fun convertToDouble(strNumber: String?): Double {
         if (strNumber.isNullOrBlank()) return 0.0
         val number = strNumber.replace(",".toRegex(), ".")
