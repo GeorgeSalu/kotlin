4d3030ab571ed364af6ad2228745f3d0ca8ddd00
George Salu <george.salu10@gmail.com>
2025-04-30 15:13:01 -0300

    spring-boot-kotlin-paths-e-exceptions - definindo as funcoes de validacao

--- spring-boot-kotlin-paths-e-exceptions/src/main/kotlin/br/com/rest/controller/MathController.kt
+++ spring-boot-kotlin-paths-e-exceptions/src/main/kotlin/br/com/rest/controller/MathController.kt
@@ -14,12 +14,16 @@ class MathController {
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
     }
 
-    private fun convertToDouble(numberOne: String?): Double {
-        return 0.0
+    private fun convertToDouble(strNumber: String?): Double {
+        if (strNumber.isNullOrBlank()) return 0.0
+        val number = strNumber.replace(",".toRegex(), ".")
+        return if (isNumeric(number)) number.toDouble() else 0.0
     }
 
-    private fun isNumeric(numberOne: String?): Boolean {
-        return false
+    private fun isNumeric(strNumber: String?): Boolean {
+        if (strNumber.isNullOrBlank()) return false
+        val number = strNumber.replace(",".toRegex(), ".")
+        return number.matches("""[-+]?[0-9]*\.?[0-9]""".toRegex())
     }
 
 }
