79bbd3278048d77646df7098db1debab4a973d13
George Salu <george.salu10@gmail.com>
2025-04-30 11:42:50 -0300

    primeiros-passos-spring-boot-kotlin - renomeando GreetinControler para GreetingController

--- primeiros-passos-spring-boot-kotlin/src/main/kotlin/br/com/rest/GreetingController.kt
+++ primeiros-passos-spring-boot-kotlin/src/main/kotlin/br/com/rest/GreetingController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
 
 @RestController
-class GreetinControler {
+class GreetingController {
 
     val counter: AtomicLong = AtomicLong()
 
