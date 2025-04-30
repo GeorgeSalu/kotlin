77d988805e1f8b218cab0345e7044308afb2e198
George Salu <george.salu10@gmail.com>
2025-04-30 14:21:55 -0300

    spring-boot-kotlin-paths-e-exceptions - definindo a logica da nossa aplicacao

--- spring-boot-kotlin-paths-e-exceptions/src/main/kotlin/br/com/rest/MathController.kt
+++ spring-boot-kotlin-paths-e-exceptions/src/main/kotlin/br/com/rest/MathController.kt
@@ -10,7 +10,7 @@ class MathController {
     @RequestMapping(value = ["/sum/{numberOne}/{numberTwo}"])
     fun sum(@PathVariable(value = "numberOne") numberOne: String?,@PathVariable(value = "numberTwo") numberTwo: String?): Double {
 
-        if(isNumeric(numberOne) || isNumeric(numberTwo)) throw Exception()
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw Exception()
         return convertToDouble(numberOne) + convertToDouble(numberTwo)
     }
 
