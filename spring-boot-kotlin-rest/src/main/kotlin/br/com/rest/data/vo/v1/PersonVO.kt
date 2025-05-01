764724baa205ac3fd037f2987e0e6d67db92a147
George Salu <george.salu10@gmail.com>
2025-05-01 13:20:33 -0300

    spring-boot-kotlin-rest - adicionando a dependencia jakarta.xml.bind

---
+++ spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/data/vo/v1/PersonVO.kt
@@ -0,0 +1,14 @@
+package br.com.rest.data.vo.v1
+
+data class PersonVO (
+
+    var id: Long = 0,
+
+    var firstName: String = "",
+
+    var lastName: String = "",
+
+    var address: String = "",
+
+    var gender: String = "",
+)
