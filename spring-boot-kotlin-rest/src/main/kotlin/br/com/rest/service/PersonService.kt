553cbd71748853cd56fefc1db636e8d4f73a1e2c
George Salu <george.salu10@gmail.com>
2025-04-30 22:27:34 -0300

    spring-boot-kotlin-rest - injecao de dependencias com @Serviec e @Autoquired

---
+++ spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/service/PersonService.kt
@@ -0,0 +1,27 @@
+package br.com.rest.service
+
+import br.com.rest.model.Person
+import org.springframework.stereotype.Service
+import java.util.concurrent.atomic.AtomicLong
+import java.util.logging.Logger
+
+@Service
+class PersonService {
+
+    val counter: AtomicLong = AtomicLong()
+
+    private val logger = Logger.getLogger(PersonService::class.java.name)
+
+    fun findById(id: Long): Person {
+        logger.info("finding one person")
+
+        val person = Person()
+        person.id = counter.incrementAndGet()
+        person.firstName = "Leandro"
+        person.lastName = "costa"
+        person.address = "minas gerais"
+        person.gender = "male"
+        return person
+    }
+
+}
