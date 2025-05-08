a080ad752280e5e7d0a42472178320d8837735c6
George Salu <george.salu10@gmail.com>
2025-05-07 21:22:24 -0300

    15-spring-boot-kotlin-rest - definindo a configuracao basica do swagger

---
+++ 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/config/OpenApiconfig.kt
@@ -0,0 +1,28 @@
+package br.com.rest.config
+
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.info.License
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class OpenApiconfig {
+
+    @Bean
+    fun customOpenApi(): OpenAPI {
+        return OpenAPI()
+            .info(
+                Info()
+                    .title("Restful api with Kotlin and Spring Boot")
+                    .version("v1")
+                    .description("Some description about your API")
+                    .termsOfService("")
+                    .license(
+                        License().name("Apache 2.0")
+                            .url("")
+                    )
+            )
+    }
+
+}
