70509c5294eea681ee10199015e7dedb3842370b
George Salu <george.salu10@gmail.com>
2025-05-08 09:53:51 -0300

    15-spring-boot-kotlin-rest - customizando a documentacao do swagger para endpoint findAll de person

--- 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/controller/PersonController.kt
+++ 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/controller/PersonController.kt
@@ -2,6 +2,12 @@ package br.com.rest.controller
 
 import br.com.rest.data.vo.v1.PersonVO
 import br.com.rest.service.PersonService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
@@ -16,12 +22,40 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/person/v1")
+@Tag(name = "People", description = "Endpoints for managing people")
 class PersonController {
 
     @Autowired
     private lateinit var service: PersonService
 
     @GetMapping(produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
+    @Operation(summary = "finds all people", description = "finds all people",
+        tags = ["people"],
+        responses = [
+            ApiResponse(
+                description = "Success",
+                responseCode = "200",
+                content = [
+                    Content(array = ArraySchema(schema = Schema(implementation = PersonVO::class)))
+                ]
+            ),
+            ApiResponse(description = "No content", responseCode = "204", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "Bad Request", responseCode = "400", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "Unauthorized", responseCode = "401", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "No Found", responseCode = "404", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "Internal Error", responseCode = "500", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ])
+        ]
+    )
     fun findAll() : List<PersonVO> {
         return service.findAll()
     }
