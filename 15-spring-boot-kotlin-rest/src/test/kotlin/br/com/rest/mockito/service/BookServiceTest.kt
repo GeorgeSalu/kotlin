9612a3cadcba6d2a6fba28ac046daa8d89f0d296
George Salu <george.salu10@gmail.com>
2025-05-13 14:39:37 -0300

    15-spring-boot-kotlin-rest - criando estrutura inicial BookServiceTest

---
+++ 15-spring-boot-kotlin-rest/src/test/kotlin/br/com/rest/mockito/service/BookServiceTest.kt
@@ -0,0 +1,31 @@
+package br.com.rest.mockito.service
+
+import br.com.rest.repository.BookRepository
+import br.com.rest.service.BookService
+import br.com.rest.unitests.mapper.mocks.MockBook
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+
+
+import org.mockito.MockitoAnnotations
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+class BookServiceTest {
+
+    private lateinit var inputObject: MockBook
+
+    @InjectMocks
+    private lateinit var service: BookService
+
+    @Mock
+    private lateinit var repository: BookRepository
+
+    @BeforeEach
+    fun setUpMock() {
+        inputObject = MockBook()
+    }
+
+}
