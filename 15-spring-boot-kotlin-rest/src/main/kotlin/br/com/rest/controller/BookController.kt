f1976425f7439d608bd3f07e46dc55cb7a528a5f
George Salu <george.salu10@gmail.com>
2025-05-13 12:43:27 -0300

    15-spring-boot-kotlin-rest - documentando o endpoint findAll books

--- 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/controller/BookController.kt
+++ 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/controller/BookController.kt
@@ -2,6 +2,11 @@ package br.com.rest.controller
 
 import br.com.rest.data.vo.v1.BookVO
 import br.com.rest.service.BookService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -21,6 +26,33 @@ class BookController {
     private lateinit var service: BookService
 
     @GetMapping
+    @Operation(summary = "Finds all Books", description = "Finds all Books",
+        tags = ["Books"],
+        responses = [
+            ApiResponse(
+                description = "Success",
+                responseCode = "200",
+                content = [
+                    Content(array = ArraySchema(schema = Schema(implementation = BookVO::class)))
+                ]
+            ),
+            ApiResponse(description = "No Content", responseCode = "204", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "Bad Request", responseCode = "400", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "Unauthorized", responseCode = "401", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "Not Found", responseCode = "404", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+            ApiResponse(description = "Internal Error", responseCode = "500", content = [
+                Content(schema = Schema(implementation = Unit::class))
+            ]),
+        ]
+    )
     fun findAll(): List<BookVO> {
         return service.findAll()
     }
