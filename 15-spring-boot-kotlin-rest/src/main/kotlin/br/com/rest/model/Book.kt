74b0a5e1934a869ca3092b99b2dbc8d41b08491d
George Salu <george.salu10@gmail.com>
2025-05-13 11:44:12 -0300

    15-spring-boot-kotlin-rest - mapeando a entidade book

---
+++ 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/model/Book.kt
@@ -0,0 +1,30 @@
+package br.com.rest.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import java.util.Date
+
+@Entity
+@Table(name = "books")
+data class Book(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @Column(nullable = false, length = 180)
+    var authos: String = "",
+
+    @Column(name = "launch_date")
+    var launchDate: Date? = null,
+
+    @Column(nullable = false)
+    var price: Double = 0.0,
+
+    @Column(nullable = false, length = 250)
+    var title: String = "",
+)
