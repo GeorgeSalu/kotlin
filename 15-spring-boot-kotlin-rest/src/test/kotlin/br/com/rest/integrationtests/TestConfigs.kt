740809346e3463b5fb09db67429225627e469212
George Salu <george.salu10@gmail.com>
2025-05-20 14:48:58 -0300

    16-spring-boot-kotlin-rest - constantes para facilitar na hora da criacao dos testes

--- 15-spring-boot-kotlin-rest/src/test/kotlin/br/com/rest/integrationtests/TestConfigs.kt
+++ 15-spring-boot-kotlin-rest/src/test/kotlin/br/com/rest/integrationtests/TestConfigs.kt
@@ -4,7 +4,7 @@ object TestConfigs {
 
     const val SERVER_PORT = 8888
     const val HEADER_PARAM_ORIGIN = "Origin"
-    const val CONTENT_TYPE_JSON = "application/sjon"
+    const val CONTENT_TYPE_JSON = "application/json"
     const val CONTENT_TYPE_XML = "application/xml"
     const val CONTENT_TYPE_YAML = "application/x-yaml"
     const val ORIGIN_ERUDIO = "https://erudio.com.br"
