269fe65a6a90a676bfdf4d4d3a573f1a0d0ec310
George Salu <george.salu10@gmail.com>
2025-05-20 12:17:03 -0300

    16-spring-boot-kotlin-rest - habilitando cors a nivel de metodo

--- 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/controller/PersonController.kt
+++ 15-spring-boot-kotlin-rest/src/main/kotlin/br/com/rest/controller/PersonController.kt
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+//@CrossOrigin
 @RestController
 @RequestMapping("/api/person/v1")
 @Tag(name = "People", description = "Endpoints for managing people")
@@ -60,6 +62,7 @@ class PersonController {
         return service.findAll()
     }
 
+    @CrossOrigin(origins = ["http://localhost:8080"])
     @GetMapping(value = ["/{id}"], produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
     @Operation(summary = "finds a people", description = "finds a people",
         tags = ["people"],
@@ -92,6 +95,7 @@ class PersonController {
         return service.findById(id)
     }
 
+    @CrossOrigin(origins = ["http://localhost:8080"])
     @PostMapping(produces = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE], consumes = [MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE])
     @Operation(summary = "adds a new person", description = "adds a new person",
         tags = ["people"],
