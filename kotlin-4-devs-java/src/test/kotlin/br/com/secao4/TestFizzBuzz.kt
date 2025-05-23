9f21a62ed5a01193055524ee65005d15ad97e70c
George Salu <george.salu10@gmail.com>
2025-05-23 11:50:02 -0300

    kotlin-4-devs-java - junit validando desafio fizzbuzz

--- kotlin-4-devs-java/src/test/kotlin/br/com/secao4/TestFizzBuzz.kt
+++ kotlin-4-devs-java/src/test/kotlin/br/com/secao4/TestFizzBuzz.kt
@@ -10,4 +10,16 @@ class TestFizzBuzz {
         fizzBuzz(5) shouldBeEqualTo listOf("1", "2", "Fizz", "4", "Buzz")
     }
 
+    @Test
+    fun `Calling fizzbuzz with 16 returns out the correct values`() {
+
+        val list = listOf(
+            "1", "2", "Fizz", "4", "Buzz", "Fizz",
+            "7", "8", "Fizz", "Buzz", "11", "Fizz",
+            "13", "14", "FizzBuzz", "16"
+        )
+
+        fizzBuzz(16) shouldBeEqualTo list
+    }
+
 }
