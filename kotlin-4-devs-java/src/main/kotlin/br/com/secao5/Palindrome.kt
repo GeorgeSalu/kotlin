004f254be8a50f8fcec0950ee2990a00cef79558
George Salu <george.salu10@gmail.com>
2025-05-23 14:26:19 -0300

    kotlin-4-devs-java - desafio palindrome exemplos

--- kotlin-4-devs-java/src/main/kotlin/br/com/secao5/Palindrome.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/secao5/Palindrome.kt
@@ -3,4 +3,8 @@ package br.com.secao5
 /*
 Given a string, return true if the string is a palindrome or false if it is not. Palindromes are strings that form the
 same word if it is reversed. Include spaces and punctuation in determining if the string is a palindrome.
+
+Examples
+palindrome("civic") // true
+palindrome("abcdefg") // false
 */
