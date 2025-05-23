d1df2608b80fe1d01eaaacab28dae6bc03af531f
George Salu <george.salu10@gmail.com>
2025-05-23 09:15:28 -0300

    kotlin-4-devs-java - tipos de variaveis em kotlin

---
+++ kotlin-4-devs-java/src/main/kotlin/br/com/secao4/TiposDeVariaveis.kt
@@ -0,0 +1,25 @@
+package br.com.secao4
+
+fun main() {
+
+    val myPhrase = "I'm a string"
+    val myPi = 3.14
+    val myAnswer = 42
+
+    val aString: String
+    val aDouble: Double
+    val aInt: Int
+
+    val myDouble = 1.999
+    val myFloat = 1.9f
+    val myLong = 999999922339999L
+    val myInt = 199
+    val myShort: Short = 12
+    val myByte: Byte = -128
+
+    val aLongNumber = 123_456_789
+    val theSameNumber = 123456789
+
+    val anInt: Int = 7
+    val aLong: Long = anInt.toLong()
+}
