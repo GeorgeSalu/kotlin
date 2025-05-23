df64ddb988b8486adff0f52883c705cea203af21
George Salu <george.salu10@gmail.com>
2025-05-23 11:45:47 -0300

    kotlin-4-devs-java - solucao 1 desafio fizzbuzz

--- kotlin-4-devs-java/src/main/kotlin/br/com/secao4/DesafioFizzBuzz.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/secao4/DesafioFizzBuzz.kt
@@ -8,3 +8,18 @@ word `Buzz`. For numbers which are multiples of both three and five list should 
 fizzBuzz(5) // [1, 2, "Fizz", 4, "Buzz"]
 fizzBuzz(16) // [1, 2, "Fizz", 4, "Buzz", 5, "Fizz", 7, 8, "Fizz", "Buzz", 11, "Fizz", 13, 14, "FizzBuzz", 16]
 * */
+
+fun fizzBuzz(n: Int): List<String> {
+    val list = mutableListOf<String>()
+
+    for (it in 1..n) {
+        val item = when {
+            it % 3 == 0 && it % 5 == 0 -> "FizzBuzz"
+            it % 3 == 0 -> "Fizz"
+            it % 5 == 0 -> "Buzz"
+            else -> it.toString()
+        }
+        list.add(item)
+    }
+    return  list
+}
