8d421bcf2a09f099f6e97eff5a46f4bc711b0c79
George Salu <george.salu10@gmail.com>
2025-05-26 09:09:29 -0300

    kotlin-4-devs-java - desafio de operator overloagin

--- kotlin-4-devs-java/src/main/kotlin/br/com/secao7/OperatorOverloadingDesafio.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/secao7/OperatorOverloadingDesafio.kt
@@ -1,5 +1,20 @@
 package br.com.secao7
 
 /*
-    Given two Data Classes with two attributes make sum and subtraction, return true if the return is equals to expected or false if it is not. Use Operator Overload to do that.
+    Given two Data Classes with two attributes make sum and subtraction, return true if the return is equals to expected or false if it is not.
+    Use Operator Overload to do that.
+
+    operator fun plus(other: Coordinates) : Coordinates {
+        TODO("not implemented")
+    }
+
+    operator fun minus (other: Coordinates) : Coordinates {
+        TODO("not implemented")
+    }
+
+    val p3 = p1 + p2
+    p3.toString() shouldBeEqualTo "Coordinates(x=1200, y=2100)" // true
+
+    val p3 = p1 - p2
+    p3.toString() shouldBeEqualTo "Coordinates(x=1200, y=2100)" // false
 * */
