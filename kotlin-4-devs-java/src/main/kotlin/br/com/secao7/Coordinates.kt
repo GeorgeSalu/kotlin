a420bd6ae849c20554abfeb031b33b7562dd2218
George Salu <george.salu10@gmail.com>
2025-05-26 09:29:58 -0300

    kotlin-4-devs-java - junit desafio de operator overloagin metodo plus

--- kotlin-4-devs-java/src/main/kotlin/br/com/secao7/Coordinates.kt
+++ kotlin-4-devs-java/src/main/kotlin/br/com/secao7/Coordinates.kt
@@ -2,8 +2,7 @@ package br.com.secao7
 
 data class Coordinates(var x: Int,var y: Int) {
 
-    operator fun plus(other: Coordinates): Coordinates {
-        return Coordinates(x + other.x, y + other.y)
-    }
+    operator fun plus(other: Coordinates): Coordinates = Coordinates(x + other.x, y + other.y)
+    operator fun minus(other: Coordinates): Coordinates = Coordinates(x - other.x, y - other.y)
 
 }
