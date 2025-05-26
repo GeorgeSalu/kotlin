ed8fc7b9d64dd9f3593585dde5c6d5736253ba95
George Salu <george.salu10@gmail.com>
2025-05-26 09:07:14 -0300

    kotlin-4-devs-java - desestruturacao

---
+++ kotlin-4-devs-java/src/main/kotlin/br/com/secao7/Desestruturacao.kt
@@ -0,0 +1,22 @@
+package br.com.secao7
+
+fun main() {
+
+    showComponents()
+
+    val c1 = Coordinates(200, 100)
+    val c2 = Coordinates(1000,2000)
+    val c3 = c1 + c2
+    println(c3)
+
+    val (xCoordinate, yCoordinate) = c3
+
+    println("xCoordinate = $xCoordinate,yCoordinate = $yCoordinate")
+    println("xCoordinate = ${c3.component1()}")
+}
+
+fun showComponents() {
+    val myPoint = Point(5000,500,60000)
+    val (myX,myY,myZ) = myPoint
+    println("myX = $myX,myY = $myY,myZ = $myZ")
+}
