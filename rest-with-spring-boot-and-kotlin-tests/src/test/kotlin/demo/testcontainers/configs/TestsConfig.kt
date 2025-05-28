9e5fa609cb4f0db4cbd228f0b65a2c8b1003fed7
George Salu <george.salu10@gmail.com>
2025-05-28 10:52:23 -0300

    rest-with-spring-boot-and-kotlin-tests - classe de constantes que sera usado nos teste

--- rest-with-spring-boot-and-kotlin-tests/src/test/kotlin/demo/testcontainers/configs/TestsConfig.kt
+++ rest-with-spring-boot-and-kotlin-tests/src/test/kotlin/demo/testcontainers/configs/TestsConfig.kt
@@ -2,6 +2,6 @@ package demo.testcontainers.configs
 
 object TestsConfig {
 
-    const val SERVER_CONFIG = 8888;
+    const val SERVER_PORT = 8888;
     const val CONTENT_TYPE = "application/json"
 }
